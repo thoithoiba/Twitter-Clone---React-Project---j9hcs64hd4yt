9661213b31f5317aadbd72b90f364db93fd437e0
thoithoiba <70210729+thoithoiba@users.noreply.github.com>
2023-03-21 23:32:42 +0530

    modified:   twitter-clone/src/index.js

--- twitter-clone/src/index.js
+++ twitter-clone/src/index.js
@@ -11,12 +11,10 @@ let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
      <PersistGate loading={null} persistor={persistor}>
 
     <App />
      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  </Provider>
 );
