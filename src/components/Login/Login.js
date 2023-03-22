ac5ea5fd0547db3115079197bd3aaf0397db6986
thoithoiba <70210729+thoithoiba@users.noreply.github.com>
2023-03-22 21:31:20 +0530

    Changes to be committed: 	modified:   src/components/Login/Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Login.css";
 import { useDispatch } from "react-redux";
 import { login } from "../../features/loginSlice";
